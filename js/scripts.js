ae610c54afac7630702821f71a5368b146460ad2
Hannah Lee and Sara Hamilton <sara@>
2018-01-10 16:50:43 -0800

    add parseInt to javascript, edit HTML under address section.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,22 @@ $(document).ready(function() {
     var noseQuantityInput = parseInt($("input#nose-quantity").val());
     var wigInput = $("#wig").val();
     var wigQuantityInput = parseInt($("input#wig-quantity").val());
+    var nameInput = $("#name").val();
+    var addressInput = $("#address").val();
+    var cityInput = $("#city").val();
+    var stateInput = $("#state").val();
+    var zipInput = $("#zip").val();
 
     $(".shoe-size").text(shoeSizeInput);
     $(".shoe-quantity").text(shoeQuantityInput);
     $(".nose-quantity").text(noseQuantityInput);
     $(".wig").text(wigInput);
     $(".wig-quantity").text(wigQuantityInput);
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
 
     $("#receipt").show();
 
